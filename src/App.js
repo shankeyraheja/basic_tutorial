be40d8d008cf111597ef70f9a77e31005cf0dd76
shankey raheja <shankeyraheja@gmail.com>
2024-08-09 13:24:53 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>NAVBAR</h1>
+      <div className='item'>
+        <div>HOME</div>
+        <div>ABOUT</div>
+        <div>CONTACT</div>
+        <div>BLOGS</div>
+      </div>
     </div>
   );
 }
